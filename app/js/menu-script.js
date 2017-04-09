c967e8c47941941be6e386c22df1becef323fb35
FC2012 <556677at@gmail.com>
2017-04-09 23:09:13 +0300

    onmouseover menu

---
+++ app/js/menu-script.js
@@ -0,0 +1,14 @@
+function mOver(obj) {
+    obj.style.background = "rgba(255,255,255,0.9)";
+    obj.firstElementChild.style = "color:rgb(217, 170, 32)";
+}
+
+function mOut(obj) {
+    obj.style.background = "rgba(51,1,2,0.8)";
+    obj.firstElementChild.style = "color:rgba(255,255,255,0.9)";
+}
+
+
+/*obj.style.background = "rgba(255,255,255,0.9)";
+obj.style.color = "darkred";
+obj.style.background = "rgba(51,1,2,0.8)";*/

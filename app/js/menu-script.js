6e676c231f3a038d49845d71866e3d4f2124ae76
FC2012 <556677at@gmail.com>
2017-04-11 16:27:32 +0300

    footer-slider

--- app/js/menu-script.js
+++ app/js/menu-script.js
@@ -12,3 +12,33 @@ function mOut(obj) {
 /*obj.style.background = "rgba(255,255,255,0.9)";
 obj.style.color = "darkred";
 obj.style.background = "rgba(51,1,2,0.8)";*/
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,3000);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}

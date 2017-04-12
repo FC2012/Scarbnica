fd1d59b23b8c677d566c80863243b9bbf03044cc
FC2012 <556677at@gmail.com>
2017-04-12 11:35:57 +0300

    adding footer feache

--- app/js/menu-script.js
+++ app/js/menu-script.js
@@ -12,3 +12,16 @@ function mOut(obj) {
 /*obj.style.background = "rgba(255,255,255,0.9)";
 obj.style.color = "darkred";
 obj.style.background = "rgba(51,1,2,0.8)";*/
+
+
+// SLIDER
+
+var slides = document.querySelectorAll('#slider-container .slider-element');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,3000);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slider-element';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slider-element active';
+}
